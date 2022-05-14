e9c2f0349495ced0f6956239d795babfdb982b9c
Isaac Chukwuemeke Oselukwue <princeizak@live.com>
2022-05-14 12:07:19 +0100

    HGG

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -85,7 +85,7 @@ function App() {
               </Button>
 
               <LinkContainer to="/">
-                <Navbar.Brand>amazona</Navbar.Brand>
+                <Navbar.Brand>AMX</Navbar.Brand>
               </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
